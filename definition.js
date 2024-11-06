ace0b5dc120156306373c17aec368742810b91cf
KIEN20030806 <kien08062003@gmail.com>
2024-11-06 14:58:34 +0700

    update

--- definition.js
+++ definition.js
@@ -10,7 +10,8 @@ Blockly.Blocks['relay_control_1'] = {
           "name": "state",
           "options": [
             ["Bật", "1"],
-            ["Tắt", "0"]
+            ["Tắt", "0"],
+            ["Đảo trạng thái", "toggle"]
           ]
         }
       ],
@@ -25,7 +26,14 @@ Blockly.Blocks['relay_control_1'] = {
 
 Blockly.Python['relay_control_1'] = function (block) {
   var state = block.getFieldValue('state');
-  var code = 'relay_driver.setRelay(0, ' + state + ')\n'; // Điều khiển Relay 1 (kênh 0)
+  var code = '';
+
+  if (state === 'toggle') {
+    code = 'relay_driver.toggleRelay(0)\n';  // Đảo trạng thái Relay 4 (kênh 3)
+  } else {
+    code = 'relay_driver.setRelay(0, ' + state + ')\n'; // Bật/Tắt Relay 4 (kênh 3)
+  }
+
   return code;
 };
 
@@ -41,7 +49,8 @@ Blockly.Blocks['relay_control_2'] = {
           "name": "state",
           "options": [
             ["Bật", "1"],
-            ["Tắt", "0"]
+            ["Tắt", "0"],
+            ["Đảo trạng thái", "toggle"]
           ]
         }
       ],
@@ -56,7 +65,14 @@ Blockly.Blocks['relay_control_2'] = {
 
 Blockly.Python['relay_control_2'] = function (block) {
   var state = block.getFieldValue('state');
-  var code = 'relay_driver.setRelay(1, ' + state + ')\n'; // Điều khiển Relay 2 (kênh 1)
+  var code = '';
+
+  if (state === 'toggle') {
+    code = 'relay_driver.toggleRelay(1)\n';  // Đảo trạng thái Relay 4 (kênh 3)
+  } else {
+    code = 'relay_driver.setRelay(1, ' + state + ')\n'; // Bật/Tắt Relay 4 (kênh 3)
+  }
+
   return code;
 };
 
@@ -72,7 +88,8 @@ Blockly.Blocks['relay_control_3'] = {
           "name": "state",
           "options": [
             ["Bật", "1"],
-            ["Tắt", "0"]
+            ["Tắt", "0"],
+            ["Đảo trạng thái", "toggle"]
           ]
         }
       ],
@@ -87,7 +104,14 @@ Blockly.Blocks['relay_control_3'] = {
 
 Blockly.Python['relay_control_3'] = function (block) {
   var state = block.getFieldValue('state');
-  var code = 'relay_driver.setRelay(2, ' + state + ')\n'; // Điều khiển Relay 3 (kênh 2)
+  var code = '';
+
+  if (state === 'toggle') {
+    code = 'relay_driver.toggleRelay(2)\n';  // Đảo trạng thái Relay 4 (kênh 3)
+  } else {
+    code = 'relay_driver.setRelay(2, ' + state + ')\n'; // Bật/Tắt Relay 4 (kênh 3)
+  }
+
   return code;
 };
 
@@ -103,7 +127,8 @@ Blockly.Blocks['relay_control_4'] = {
           "name": "state",
           "options": [
             ["Bật", "1"],
-            ["Tắt", "0"]
+            ["Tắt", "0"],
+            ["Đảo trạng thái", "toggle"]
           ]
         }
       ],
@@ -118,7 +143,14 @@ Blockly.Blocks['relay_control_4'] = {
 
 Blockly.Python['relay_control_4'] = function (block) {
   var state = block.getFieldValue('state');
-  var code = 'relay_driver.setRelay(3, ' + state + ')\n'; // Điều khiển Relay 4 (kênh 3)
+  var code = '';
+
+  if (state === 'toggle') {
+    code = 'relay_driver.toggleRelay(3)\n';  // Đảo trạng thái Relay 4 (kênh 3)
+  } else {
+    code = 'relay_driver.setRelay(3, ' + state + ')\n'; // Bật/Tắt Relay 4 (kênh 3)
+  }
+
   return code;
 };
 
@@ -134,7 +166,8 @@ Blockly.Blocks['relay_control_4_channels'] = {
           "name": "state_1",
           "options": [
             ["Bật", "1"],
-            ["Tắt", "0"]
+            ["Tắt", "0"],
+            ["Đảo trạng thái", "toggle"]
           ]
         },
         {
@@ -142,7 +175,8 @@ Blockly.Blocks['relay_control_4_channels'] = {
           "name": "state_2",
           "options": [
             ["Bật", "1"],
-            ["Tắt", "0"]
+            ["Tắt", "0"],
+            ["Đảo trạng thái", "toggle"]
           ]
         },
         {
@@ -150,7 +184,8 @@ Blockly.Blocks['relay_control_4_channels'] = {
           "name": "state_3",
           "options": [
             ["Bật", "1"],
-            ["Tắt", "0"]
+            ["Tắt", "0"],
+            ["Đảo trạng thái", "toggle"]
           ]
         },
         {
@@ -158,7 +193,8 @@ Blockly.Blocks['relay_control_4_channels'] = {
           "name": "state_4",
           "options": [
             ["Bật", "1"],
-            ["Tắt", "0"]
+            ["Tắt", "0"],
+            ["Đảo trạng thái", "toggle"]
           ]
         }
       ],
@@ -178,10 +214,85 @@ Blockly.Python['relay_control_4_channels'] = function (block) {
   var state_4 = block.getFieldValue('state_4');
   
   var code = '';
-  code += 'relay_driver.setRelay(0, ' + state_1 + ')\n'; // Điều khiển Relay 1 (kênh 0)
-  code += 'relay_driver.setRelay(1, ' + state_2 + ')\n'; // Điều khiển Relay 2 (kênh 1)
-  code += 'relay_driver.setRelay(2, ' + state_3 + ')\n'; // Điều khiển Relay 3 (kênh 2)
-  code += 'relay_driver.setRelay(3, ' + state_4 + ')\n'; // Điều khiển Relay 4 (kênh 3)
+
+  // Xử lý Relay 1
+  if (state_1 === 'toggle') {
+    code += 'relay_driver.toggleRelay(0)\n'; // Đảo trạng thái Relay 1 (kênh 0)
+  } else {
+    code += 'relay_driver.setRelay(0, ' + state_1 + ')\n'; // Bật/Tắt Relay 1 (kênh 0)
+  }
+
+  // Xử lý Relay 2
+  if (state_2 === 'toggle') {
+    code += 'relay_driver.toggleRelay(1)\n'; // Đảo trạng thái Relay 2 (kênh 1)
+  } else {
+    code += 'relay_driver.setRelay(1, ' + state_2 + ')\n'; // Bật/Tắt Relay 2 (kênh 1)
+  }
+
+  // Xử lý Relay 3
+  if (state_3 === 'toggle') {
+    code += 'relay_driver.toggleRelay(2)\n'; // Đảo trạng thái Relay 3 (kênh 2)
+  } else {
+    code += 'relay_driver.setRelay(2, ' + state_3 + ')\n'; // Bật/Tắt Relay 3 (kênh 2)
+  }
+
+  // Xử lý Relay 4
+  if (state_4 === 'toggle') {
+    code += 'relay_driver.toggleRelay(3)\n'; // Đảo trạng thái Relay 4 (kênh 3)
+  } else {
+    code += 'relay_driver.setRelay(3, ' + state_4 + ')\n'; // Bật/Tắt Relay 4 (kênh 3)
+  }
+  
+  return code;
+};
+
+
+// Khối điều khiển Relay với các tùy chọn Bật/Tắt/Đảo trạng thái
+Blockly.Blocks['relay_toggle_control'] = {
+  init: function () {
+    this.jsonInit({
+      "type": "relay_toggle_control",
+      "message0": "Relay %1 %2",
+      "args0": [
+        {
+          "type": "field_dropdown",
+          "name": "relay",
+          "options": [
+            ["1", "0"],
+            ["2", "1"],
+            ["3", "2"],
+            ["4", "3"]
+          ]
+        },
+        {
+          "type": "field_dropdown",
+          "name": "state",
+          "options": [
+            ["Bật", "1"],
+            ["Tắt", "0"],
+            ["Đảo trạng thái", "toggle"]
+          ]
+        }
+      ],
+      "previousStatement": null,
+      "nextStatement": null,
+      "colour": "#18820c",
+      "tooltip": "Điều khiển Relay với các tùy chọn Bật/Tắt/Đảo trạng thái",
+      "helpUrl": ""
+    });
+  }
+};
+
+Blockly.Python['relay_toggle_control'] = function (block) {
+  var relay = block.getFieldValue('relay');
+  var state = block.getFieldValue('state');
+  
+  // Kiểm tra trạng thái là "Đảo trạng thái"
+  if (state === "toggle") {
+    var code = 'relay_driver.toggleRelay(' + relay + ')\n';  // Giả sử có phương thức toggleRelay
+  } else {
+    var code = 'relay_driver.setRelay(' + relay + ', ' + state + ')\n';  // Bật/Tắt theo trạng thái
+  }
   
   return code;
 };
