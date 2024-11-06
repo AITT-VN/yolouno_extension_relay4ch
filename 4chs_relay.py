d4075895b946aa6df6bff91d0e6cd5749ab8f0e6
KIEN20030806 <kien08062003@gmail.com>
2024-11-06 16:47:58 +0700

    f

--- 4chs_relay.py
+++ 4chs_relay.py
@@ -13,9 +13,14 @@ i2c = SoftI2C(scl=SCL_PIN, sda=SDA_PIN, freq=100000)
 
 channel_state = 0 
 
-def get_channel_state():
+def get_channel_state(channel):
     global channel_state
-    return channel_state
+    # Kiểm tra xem kênh đã bật hay chưa dựa vào bit của channel
+    if channel_state & (1 << (channel - 1)):  # Kiểm tra bit tương ứng với channel
+        return 1  # Kênh bật
+    else:
+        return 0  # Kênh tắt
+
 
 def channel_control(state):
     global channel_state
@@ -34,6 +39,10 @@ def turn_on_channel(channel):
 
 def toggle_channel(channel):
     global channel_state
-    channel_state ^= (1 << (channel - 1))  
-    print(f"Trạng thái kênh sau khi đảo: {bin(channel_state)}")
+    current_state = get_channel_state(channel)
+    if current_state == 1:
+        channel_state &= ~(1 << (channel - 1))  # Tắt kênh
+    else:
+        channel_state |= (1 << (channel - 1))  # Bật kênh    
     i2c.writeto_mem(i2c_addr, CMD_CHANNEL_CTRL, bytes([channel_state]))
+
