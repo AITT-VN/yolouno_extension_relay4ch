e8cac070a2f55e6e376e2e5066c7aad8cef83479
KIEN20030806 <kien08062003@gmail.com>
2024-11-08 00:41:53 +0700

    new

--- definition.js
+++ definition.js
@@ -234,7 +234,7 @@ Blockly.Python['read_relay_status_at_address'] = function (block) {
 
   var relay_code = (relay == "tất cả") ? '0' : relay;  // Chọn relay cụ thể hoặc tất cả
   
-  //var code = `relay_${address}.get_relay(${relay_code})\n`;  // Đọc trạng thái relay
+  var code = `relay_${address}.get_relay(${relay_code})\n`;  // Đọc trạng thái relay
   
   return [code, Blockly.Python.ORDER_ATOMIC];
 };
