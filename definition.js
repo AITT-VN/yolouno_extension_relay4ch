a543a6cc157cf9e75aaefb396618cd36a512b440
KIEN20030806 <kien08062003@gmail.com>
2024-11-07 15:13:52 +0700

    o

--- definition.js
+++ definition.js
@@ -183,7 +183,7 @@ Blockly.Python['control_relay_at_address'] = function (block) {
     code = `relay_${address}.toggle_relay(${address}, ${relay_code})\n`;  // Toggle
   } else {
     var state_value = (state === "1") ? '1' : '0';  // Chuyển đổi trạng thái bật/tắt
-    code = `relay_${address}.set_relay(${address}, ${relay_code}, ${state_value})\n`;  // Bật/Tắt
+    code = `relay_${address}.set_relay(${relay_code}, ${state_value})\n`;  // Bật/Tắt
   }
 
   return code;
@@ -227,6 +227,6 @@ Blockly.Python['read_relay_status_at_address'] = function (block) {
   Blockly.Python.definitions_['create_relay_driver'] = 'relay_' + address + ' = RelayController(' + address + ')';
   var code = "";
   var relay_code = (relay == "tất cả") ? '0' : relay;  
-  var code = `relay_${address}.get_relay(${address}, ${relay_code})\n`;
+  var code = `relay_${address}.get_relay(${relay_code})\n`;
   return [code, Blockly.Python.ORDER_ATOMIC];
 };
