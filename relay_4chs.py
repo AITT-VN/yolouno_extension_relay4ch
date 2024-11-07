198cd712402992a1a00733c3f06ecbf18f47b790
KIEN20030806 <kien08062003@gmail.com>
2024-11-07 14:38:26 +0700

    o

--- relay_4chs.py
+++ relay_4chs.py
@@ -12,8 +12,8 @@ class RelayController:
         self.addr = addr
         self.channel_state = 0x00
     
-    def change_relay_address(self, old_addr, new_addr):
-        self.i2c.writeto_mem(self.old_addr, self.CMD_SAVE_I2C_ADDR, bytes([new_addr]))
+    def change_relay_address(self, new_addr):
+        self.i2c.writeto_mem(self.addr, self.CMD_SAVE_I2C_ADDR, bytes([new_addr]))
 
 
     def get_relay(self, index):
